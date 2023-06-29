6cf23a7b2ea21288633f93132cb5b3addae9f8e4
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-06-29 20:56:30 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
+# Getting
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/Jengkolonline/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Jengkol_Online"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282372139631"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
 clear
-ipes=$(curl -sS ipv4.icanhazip.com)
-surat=$(curl -sS https://raw.githubusercontent.com/Jengkolonline/izinn/main/ip | grep -w $ipes | awk '{print $4}')
-red() { echo -e "\\033[31;1m${*}\\033[0m"; }
-if [[ "$surat" = "true" ]]; then
-  echo -ne
-else
-  red "You cant use this bot panel !"
-  exit 0
-fi
 
 [[ ! -f /usr/bin/jq ]] && {
   red "Downloading jq file!"

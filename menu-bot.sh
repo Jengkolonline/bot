75ba8fc0fb915cc23cc0c8145620a28133301e4b
Jengkolonline <134737979+Jengkolonline@users.noreply.github.com>
2023-07-21 02:10:33 +0700

    Update menu-bot.sh

--- menu-bot.sh
+++ menu-bot.sh
@@ -38,8 +38,8 @@ echo -e " \033[35m│$NC [05]${NC} \033[0;36m Chek User Bot Trojan${NC}"
 echo -e " \033[35m│$NC [06]${NC} \033[0;36m Daftar BOT SSH${NC}"
 echo -e " \033[35m│$NC [07]${NC} \033[0;36m Bacup BOT${NC}"
 echo -e " \033[35m│$NC [08]${NC} \033[0;36m Restart BOT${NC}"
-echo -e " \033[35m│$NC [07]${NC} \033[0;36m Add BOT Notif${NC}"
-echo -e " \033[35m│$NC [08]${NC} \033[0;36m Delet BOT Notif${NC}"
+echo -e " \033[35m│$NC [09]${NC} \033[0;36m Add BOT Notif${NC}"
+echo -e " \033[35m│$NC [10]${NC} \033[0;36m Delet BOT Notif${NC}"
 echo -e " \033[36m╰══════════════════════════════════════════════════════════╯\e[0m"
 echo -e " \033[31m##########\033[33m##########\033[32m##########\033[34m##########\033[35m##########\033[36m##########\e[0m"
 echo -e ""
@@ -54,6 +54,8 @@ case $opt in
 6) clear ; apt update && apt install wget -y && wget https://raw.githubusercontent.com/Jengkolonline/bot/main/xolpanel.sh && chmod +x xolpanel.sh && ./xolpanel.sh ; exit ;;
 7) clear ; menu-backupp ; exit ;;
 8) clear ; restorebot ; exit ;;
+9) clear ; add-bot-notif ; exit ;;
+10) clear ; del-bot-notif ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo "Anda salah tekan " ; sleep 1 ; menu-ssh ;;
